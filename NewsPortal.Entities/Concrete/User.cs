8462b2b500036fd309d630bae50fc6e5ebab4590
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-29 13:03:47 +0300

    Role ve User eklendi

---
+++ NewsPortal.Entities/Concrete/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Shared.Entities.Abstract;
+
+namespace NewsPortal.Entities.Concrete
+{
+    public class User : EntityBase, IEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string Username { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public ICollection<Report> Reports { get; set; }
+    }
+}
