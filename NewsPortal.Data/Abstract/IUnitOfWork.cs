5c85c04b2746a5a5552f3b25d4f8af8e62111d19
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-29 13:43:37 +0300

    IUnitOfWork eklendi

---
+++ NewsPortal.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReportRepository Reports { get; } //unitofworks.Reports
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } //_unitOfWork.Users.AddAsync();
+        Task<int> SaveAsync();
+    }
+}
