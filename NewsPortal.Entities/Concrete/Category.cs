b9f2ef08340ed0e5ad7c16d9ed2012fdaf49a650
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-29 13:01:05 +0300

    Category ve Report eklendi

---
+++ NewsPortal.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Shared.Entities.Abstract;
+
+namespace NewsPortal.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Report> Reports { get; set; }
+    }
+}
