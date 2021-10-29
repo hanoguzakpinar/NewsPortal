bf96c3367a653c90825ea58bbd7cc70bf671dcd7
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-29 13:05:52 +0300

    Comment eklendi

---
+++ NewsPortal.Entities/Concrete/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Shared.Entities.Abstract;
+
+namespace NewsPortal.Entities.Concrete
+{
+    public class Comment : EntityBase, IEntity
+    {
+        public string Text { get; set; }
+        public int ReportId { get; set; }
+        public Report Report { get; set; }
+    }
+}
