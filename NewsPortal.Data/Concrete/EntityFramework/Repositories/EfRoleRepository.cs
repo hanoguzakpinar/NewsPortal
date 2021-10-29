b780a4d9dee4b031f808ba0ea892e75e35bb3308
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-29 13:24:08 +0300

    concrete repolar eklendi

---
+++ NewsPortal.Data/Concrete/EntityFramework/Repositories/EfRoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewsPortal.Data.Abstract;
+using NewsPortal.Entities.Concrete;
+using NewsPortal.Shared.Data.Concrete.EntityFramework;
+
+namespace NewsPortal.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfRoleRepository : EfEntityRepositoryBase<Role>, IRoleRepository
+    {
+        public EfRoleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ NewsPortal.Data/Concrete/EntityFramework/Repositories/EfUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewsPortal.Data.Abstract;
+using NewsPortal.Entities.Concrete;
+using NewsPortal.Shared.Data.Concrete.EntityFramework;
+
+namespace NewsPortal.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfUserRepository : EfEntityRepositoryBase<User>, IUserRepository
+    {
+        public EfUserRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
