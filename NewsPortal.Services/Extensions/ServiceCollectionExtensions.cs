3d791e9d85ce043dcd192fea22d0ddcc62da5cb8
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-30 14:23:34 +0300

    loadmyservices();

---
+++ NewsPortal.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NewsPortal.Data.Abstract;
+using NewsPortal.Data.Concrete.EntityFramework.Contexts;
+using NewsPortal.Services.Abstract;
+using NewsPortal.Services.Concrete;
+
+namespace NewsPortal.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<NewsPortalContext>();
+            serviceCollection.AddScoped<IUnitOfWork, IUnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IReportService, ReportManager>();
+
+            return serviceCollection;
+        }
+    }
+}
