07424e1db43cd89b02e91b69c0b6d2acd0f11e01
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-30 18:52:05 +0300

    refactoring

--- NewsPortal.Services/Abstract/ICategoryService.cs
+++ NewsPortal.Services/Abstract/ICategoryService.cs
@@ -14,8 +14,8 @@ namespace NewsPortal.Services.Abstract
         Task<IDataResult<CategoryListDto>> GetAll();
         Task<IDataResult<CategoryListDto>> GetAllNonDeleted();
         Task<IDataResult<CategoryListDto>> GetAllNonDeletedAndActive();
-        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
-        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IDataResult<CategoryDto>> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IDataResult<CategoryDto>> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         Task<IResult> Delete(int categoryId, string modifiedByName);
         Task<IResult> HardDelete(int categoryId);
     }
