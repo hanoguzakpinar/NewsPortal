a56e7475ff2fadf81265a1cbc74c2b43299f3994
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-11-02 17:26:00 +0300

    menu componente çevrildi

---
+++ NewsPortal.Mvc/Areas/Admin/ViewComponents/AdminMenuViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using NewsPortal.Entities.Concrete;
+using NewsPortal.Mvc.Areas.Admin.Models;
+
+namespace NewsPortal.Mvc.Areas.Admin.ViewComponents
+{
+    public class AdminMenuViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AdminMenuViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            //HttpContext.User ile login olmuş kullanıcıya ulaşabiliyoruz.
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            var roles = _userManager.GetRolesAsync(user).Result;
+            return View(new UserWithRolesViewModel
+            {
+                User = user,
+                Roles = roles
+            });
+        }
+    }
+}
