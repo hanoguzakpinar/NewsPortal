4bfcb1ce938009be135b416c711fae66ff350848
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-11-02 17:38:15 +0300

    logout eklendi

--- NewsPortal.Mvc/Areas/Admin/Controllers/UserController.cs
+++ NewsPortal.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -80,6 +80,14 @@ namespace NewsPortal.Mvc.Areas.Admin.Controllers
                 return View("UserLogin");
             }
         }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home", new { Area = "" });
+        }
+
         [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<JsonResult> GetAllUsers()
