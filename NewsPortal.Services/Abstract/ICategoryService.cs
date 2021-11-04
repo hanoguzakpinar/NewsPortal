f2f90998f6b646092c23579b998e0fe9e01585fb
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-11-04 14:10:12 +0300

    xml yorum satırları

--- NewsPortal.Services/Abstract/ICategoryService.cs
+++ NewsPortal.Services/Abstract/ICategoryService.cs
@@ -11,10 +11,21 @@ namespace NewsPortal.Services.Abstract
     public interface ICategoryService
     {
         Task<IDataResult<CategoryDto>> GetAsync(int categoryId);
+        /// <summary>
+        /// Verilen id parametresine ait kategorinin CategoryUpdateDto temsilini geriye döner.
+        /// </summary>
+        /// <param name="categoryId">0'dan büyük integer bir Id değeri.</param>
+        /// <returns>Asenkron bir operasyon ile Task olarak DataResult döndürür.</returns>
         Task<IDataResult<CategoryUpdateDto>> GetCategoryUpdateDtoAsync(int categoryId);
         Task<IDataResult<CategoryListDto>> GetAllAsync();
         Task<IDataResult<CategoryListDto>> GetAllNonDeletedAsync();
         Task<IDataResult<CategoryListDto>> GetAllNonDeletedAndActiveAsync();
+        /// <summary>
+        /// Parametreleri alarak yeni bir Category ekler.
+        /// </summary>
+        /// <param name="categoryAddDto"></param>
+        /// <param name="createdByName">string tipinde kullanıcı adı</param>
+        /// <returns>Asenkron Task olarak DataResult geri döndürür.</returns>
         Task<IDataResult<CategoryDto>> AddAsync(CategoryAddDto categoryAddDto, string createdByName);
         Task<IDataResult<CategoryDto>> UpdateAsync(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         Task<IDataResult<CategoryDto>> DeleteAsync(int categoryId, string modifiedByName);
