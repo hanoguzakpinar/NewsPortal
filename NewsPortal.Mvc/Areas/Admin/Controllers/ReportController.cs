9c90f719afa83a2f92c460b1d2e6a541366f5ca2
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-11-04 14:47:34 +0300

    reportindex eklendi

---
+++ NewsPortal.Mvc/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Services.Abstract;
+using NewsPortal.Shared.Utilities.Results.ComplexTypes;
+
+namespace NewsPortal.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReportController : Controller
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _reportService.GetAllNonDeletedAsync();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
