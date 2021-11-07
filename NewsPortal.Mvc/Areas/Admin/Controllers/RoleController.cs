054432ad12095ba6285e8e34b01dad8b08d2a1a1
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-11-07 13:24:44 +0300

    rolecontroller eklendi

---
+++ NewsPortal.Mvc/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsPortal.Entities.Concrete;
+using NewsPortal.Entities.Dtos;
+
+namespace NewsPortal.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<Role> _roleManager;
+
+        public RoleController(RoleManager<Role> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "SuperAdmin,Role.Read")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(new RoleListDto
+            {
+                Roles = roles
+            });
+        }
+        [Authorize(Roles = "SuperAdmin,Role.Read")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            var roleListDto = JsonSerializer.Serialize(new RoleListDto
+            {
+                Roles = roles
+            });
+            return Json(roleListDto);
+        }
+    }
+}
